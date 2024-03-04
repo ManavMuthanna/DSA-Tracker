6fe5c608a36948ef5c61e2885b5d86817e326f6e
ManavMuthanna <muthannamanav2003@gmail.com>
2024-03-04 22:46:18 +0530

    fix: added github repo link

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -6,7 +6,7 @@ function Navbar () {
             <h1>DSA Tracker</h1>
              <div className="tags">   
             <a href="#"><h2>Team</h2></a>
-            <a href="#"><h2>Code</h2></a>
+            <a href="https://github.com/ManavMuthanna/DSA-Tracker" target="_blank"><h2>Code</h2></a>
             </div>
         </div>
     );
