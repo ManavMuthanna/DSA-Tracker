5d4dbe1f0ba00a4e466ec91bdc63e9ec63a7d1fc
ManavMuthanna <muthannamanav2003@gmail.com>
2024-03-08 02:49:36 +0530

    fix: cannot access Home without login token

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,33 @@
 import "./App.css";
-import {
-  BrowserRouter, Routes, Route} from "react-router-dom";
+import {Navigate, BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './screens/homepage/home';
 import Signup from "./screens/signup/sign_up";
 import Login from "./screens/login/login_screen";
 
 function App() {
+  const isAuthenticated = () => {
+    // Check if the token is present in local storage or any other authentication logic
+    const token = localStorage.getItem('token');
+    return token !== null;
+  };
+
+  const PrivateRoute = ({ element, path }) => {
+    return isAuthenticated() ? (
+      element
+    ) : (
+      <Navigate replace to="/" state={{ from: path }} />
+    );
+  };
+
   return (
     // <Home></Home>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={<PrivateRoute element={<Home />} path="/home" />}
+        />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
